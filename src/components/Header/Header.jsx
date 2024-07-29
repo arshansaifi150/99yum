ca21eca77ba4dca0576dc8f0c46d41496a53dedc
Mohd Arshan <110154295+arshansaifi150@users.noreply.github.com>
2024-07-29 21:58:02 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -71,7 +71,7 @@ function Header() {
               <li key={item.name}>
                 <button
                 onClick={() => navigate(item.slug)}
-                className={`inline-bock px-6 py-2 duration-200 hover:bg-orange-400 rounded-full `}
+                className={`inline-bock px-6 py-2 duration-200 hover:bg-orange-400 rounded-full font-semibold `}
                 >{item.name}</button>
               </li>
             ) : null
